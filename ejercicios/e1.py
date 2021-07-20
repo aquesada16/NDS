88af37d0f80a8c52345db67fe9308714b8e4ca9a
Alonso Quesada <aquesada@ndscognitivelabs.com>
2021-07-20 11:50:45 -0500

    segundo commit

--- ejercicios/e1.py
+++ ejercicios/e1.py
@@ -1,1 +1,11 @@
-print("Hola")
+from contextlib import contextmanager
+from tkinter import Message
+import pyautogui
+import time
+Message = 6
+while True > 0:
+    time.sleep(2)
+    pyautogui.typewrite("vente a cenar")
+    time.sleep(1)
+    pyautogui.press("enter")
+Message = Message - 1
